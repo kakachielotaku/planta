76914289e29c245b9b56d5e1d0b41807c1c1d628
kakachielotaku <88722157+kakachielotaku@users.noreply.github.com>
2021-09-24 08:56:54 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,10 @@
-basic.showLeds(`
-    . # . # .
-    . # . # .
-    . . . . .
-    . # # # .
-    # . . . #
-    `)
-if (true) {
-    basic.showString("esta todo con calma ")
-    basic.pause(100)
-    while (false) {
-        basic.showString("se te olbido regarme otra ves")
-    }
-}
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        # . . . #
+        . # # # .
+        # . . . #
+        `)
+    basic.showString("Sete olvido regarme otarves")
 })
