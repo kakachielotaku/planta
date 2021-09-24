a1955400dca14fecd509285a9acc22fc27223136
kakachielotaku <88722157+kakachielotaku@users.noreply.github.com>
2021-09-24 08:57:16 -0500

    Restaurar '21/9/2021 8:12:07 Archivos iniciales para el proyecto MakeCode'

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+if (true) {
+    basic.showString("esta todo con calma ")
+    basic.pause(100)
+    while (false) {
+        basic.showString("se te olbido regarme otra ves")
+    }
+}
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        # . . . #
-        . # # # .
-        # . . . #
-        `)
-    basic.showString("Sete olvido regarme otarves")
+	
 })
